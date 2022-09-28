1579148e3ec750b54124eaa7f5eb9188b54ecaa2
S0624 <fko2247078@stu.o-hara.ac.jp>
2022-09-28 09:58:01 +0900

    playerのdraw部分変更

--- player.cpp
+++ player.cpp
@@ -39,8 +39,13 @@ void Player::update()
 
 void Player::draw()
 {
-	if (m_isDead)	return;
-	DrawGraphF(m_pos.x, m_pos.y, m_handle, true);
+	if (m_isDead) 
+	{
+	 	DrawRectGraphF(m_pos.x, m_pos.y, m_graphSize.x / 2, 0, m_graphSize.x / 2, m_graphSize.y, m_handle, true);
+		return;
+	}
+
+ 	DrawRectGraphF(m_pos.x, m_pos.y, 0, 0, m_graphSize.x / 2, m_graphSize.y, m_handle, true);
 }
 
 bool Player::isCol(Car& car)
